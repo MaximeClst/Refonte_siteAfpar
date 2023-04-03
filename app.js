453ecb3dfa621cb0c79de88cecb450963ac23a8b
Maxime <maxime.celeste1221@gmail.com>
2023-04-03 08:27:33 +0400

    Modif carousel

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
 // // FONCTION POUR FLIP LES CARDS
-// function flipCard(){
+const card = document.querySelector(".card__inner");
 
-// const card = document.querySelectorAll(".card__inner");
+card.addEventListener("click", function (e) {
+  card.classList.toggle('is-flipped');
+});
 
-// card__inner.addEventListener("click", function (e) {
-//   for(i = 0; i < '.card__inner'; i++) {
-//     card = '.card__inner' + i;
-//   }
-//   card.classList.toggle("is-flipped");
-// })
-// }
 
 
 // FONCTION POUR FAIRE REMPLACER L'IMAGE PAR LE TEXTE LORS DU FLIP
-function gradient(id, level)
-{
-	var box = document.getElementById(id);
-	box.style.opacity = level;
-	box.style.MozOpacity = level;
-	box.style.KhtmlOpacity = level;
-	box.style.filter = "alpha(opacity=" + level * 100 + ")";
-	box.style.display="block";
-	return;
-}
+// function gradient(id, level)
+// {
+// 	var box = document.getElementById(id);
+// 	box.style.opacity = level;
+// 	box.style.MozOpacity = level;
+// 	box.style.KhtmlOpacity = level;
+// 	box.style.filter = "alpha(opacity=" + level * 100 + ")";
+// 	box.style.display="block";
+// 	return;
+// }
 
-function fadein(id) 
-{
-    var level = 0;
-    while(level <= 1)
-    {
-        setTimeout( "gradient('" + id + "'," + level + ")", (level* 1000) + 10);
-        level += 0.01;
-    }
-}
+// function fadein(id) 
+// {
+//     var level = 0;
+//     while(level <= 1)
+//     {
+//         setTimeout( "gradient('" + id + "'," + level + ")", (level* 1000) + 10);
+//         level += 0.01;
+//     }
+// }
 
-function showBack()
-{
-    fadein("card__body");
-} 
+// function showBack()
+// {
+//     fadein("card__body");
+// } 
